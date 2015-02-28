66284e4908d4e338400b0959061fccd618dd6762
Techial <techial2@gmail.com>
2015-02-28 01:04:54 +0100

    Added functions to GUI.js to allow more flexibility, and give more control to the user.

--- app/JS/GUI.js
+++ app/JS/GUI.js
@@ -1,18 +1,54 @@
+function disableTitle(){
+	$("menuTitle").hide();
+}
+
+function disableMinButton(){
+	$(".minButton").hide();
+}
+
+function disableExitButton(){
+	$(".exitButton").hide();
+}
+
+function disableMenu(){
+	$("#menu").hide();
+	$("#displayContainer").css("padding-top", "0");
+}
+
+function activateMenu(){
+	$("#menu").show();
+	$("#displayContainer").css("padding-top", "35px");
+}
+
+function activateTitle(){
+	activateMenu();
+	$("menuTitle").show();
+}
+
+function activateMinButton(){
+	activateMenu();
+	$(".minButton").show();
+}
+
+function activateExitButton(){
+	activateMenu();
+	$(".exitButton").show();
+}
+
 function refreshGUI(){
 	if(!GUI_title){
-		$("menuTitle").hide();
+		disableTitle();
 	}
 
 	if(!GUI_minimizeButton){
-		$(".minButton").hide();
+		disableMinButton();
 	}
 
 	if(!GUI_exitButton){
-		$(".exitButton").hide();
+		disableExitButton();
 	}
 	
 	if(!GUI_menu){
-		$("#menu").hide();
-		$("#displayContainer").css("padding-top", "0");
+		disableMenu();
 	}
 }
